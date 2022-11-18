4704ac942a4bc4c7f86ff3ca465761d4e4b15b36
Hwang <ggg06139@gmail.com>
2022-11-19 01:23:54 +0900

    Feat: "입력 페이지 제작, CRUD 기능 구현"
    홈페이지에서 floatingActionButton 클릭 시 이동되는 입력 페이지 제작 / C: 입력 페이지에서 입력한 값이 홈페이지에서 생성되는 기능 구현 / RUD: 홈페이지에서 입력값을 읽고 업데이트하고 지우는 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:introduction_screen/introduction_screen.dart';
@@ -90,14 +91,29 @@ class OnboardingPage extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+/// 버킷 클래스
+class Bucket {
+  String job; // 할 일
+  bool isDone; // 완료 여부
+
+  Bucket(this.job, this.isDone); // 생성자
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Bucket> bucketList = []; // 전체 버킷리스트 목록
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HomePage"),
+        title: Text("버킷 리스트"),
         actions: [
           // 삭제 버튼
           IconButton(
@@ -109,11 +125,158 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-          child: Text(
-        "환영합니다.",
-        style: TextStyle(fontSize: 25),
-      )),
+      body: bucketList.isEmpty
+          ? Center(child: Text("버킷 리스트를 작성해 주세요."))
+          : ListView.builder(
+              itemCount: bucketList.length, // bucketList 개수 만큼 보여주기
+              itemBuilder: (context, index) {
+                Bucket bucket = bucketList[index]; // index에 해당하는 bucket 가져오기
+                return ListTile(
+                  // 버킷 리스트 할 일
+                  title: Text(
+                    bucket.job,
+                    style: TextStyle(
+                      color: bucket.isDone ? Colors.grey : Colors.black,
+                      decoration: bucket.isDone
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                      fontSize: 24,
+                    ),
+                  ),
+                  // 삭제 아이콘 버튼
+                  trailing: IconButton(
+                    icon: Icon(CupertinoIcons.delete),
+                    onPressed: () {
+                      // 삭제 버튼 클릭시
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("정말로 삭제하시겠습니까?"),
+                            actions: [
+                              // 취소 버튼
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("취소"),
+                              ),
+                              // 확인 버튼
+                              TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    bucketList.removeAt(index);
+                                  });
+                                  Navigator.pop(context);
+                                },
+                                child: Text(
+                                  "확인",
+                                  style: TextStyle(color: Colors.pink),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                  onTap: () {
+                    // 아이템 클릭시
+                    setState(() {
+                      bucket.isDone = !bucket.isDone;
+                    });
+                  },
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          // + 버튼 클릭시 버킷 생성 페이지로 이동
+          String? job = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => CreatePage()),
+          );
+          if (job != null) {
+            setState(() {
+              Bucket newBucket = Bucket(job, false);
+              bucketList.add(newBucket);
+            });
+          }
+        },
+      ),
+    );
+  }
+}
+
+/// 버킷 생성 페이지
+class CreatePage extends StatefulWidget {
+  const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  // TextField의 값을 가져올 때 사용합니다.
+  TextEditingController textController = TextEditingController();
+  String? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("버킷리스트 작성"),
+        // 뒤로가기 버튼
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.chevron_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // 텍스트 입력창
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration:
+                  InputDecoration(hintText: "하고 싶은 일을 입력하세요", errorText: error),
+            ),
+            SizedBox(height: 32),
+            // 추가하기 버튼
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                child: Text(
+                  "추가하기",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  String job = textController.text;
+                  if (job.isEmpty) {
+                    setState(() {
+                      error = "내용을 입력해주세요.";
+                    });
+                  } else {
+                    setState(() {
+                      error = null;
+                    });
+                    Navigator.pop(context, job);
+                  }
+                  // 추가하기 버튼 클릭시
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
